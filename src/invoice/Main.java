bd5c77950d794c3cfe10cf8e234f18e2049625a3
Deepika22002 <85239175+Deepika22002@users.noreply.github.com>
2022-09-21 22:39:43 +0530

    Update Main.java

--- src/invoice/Main.java
+++ src/invoice/Main.java
@@ -1,2 +1,13 @@
-package invoice;public class Main {
+package invoice;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args){
+        DBfunction db=new DBfunction();
+        Connection conn= db.connect_to_db("Invoice","postgres","deepika");
+       db.createTable(conn,"Items");
+       db.rowinsert(conn,"Items","xyz",7,100);
+       db.read_data(conn,"Items");
+    }
 }
